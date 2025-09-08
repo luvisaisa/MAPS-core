060db7207e7399747d6339a2944913543ec9fb07
luckyisaisa <isa.lucia.sch@gmail.com>
2025-09-08 09:00:00 -0400

    feat: add public api exports to package init

--- src/maps/__init__.py
+++ src/maps/__init__.py
@@ -1,3 +1,22 @@
-"""MAPS - Medical Annotation Processing System"""
+"""MAPS - Medical Annotation Processing System
 
-__version__ = "0.1.0"
+Core functionality for parsing medical imaging XML annotation data.
+"""
+
+__version__ = "0.2.0"
+
+from .parser import (
+    parse_radiology_sample,
+    parse_multiple,
+    export_excel,
+    detect_parse_case,
+    get_expected_attributes_for_case
+)
+
+__all__ = [
+    'parse_radiology_sample',
+    'parse_multiple',
+    'export_excel',
+    'detect_parse_case',
+    'get_expected_attributes_for_case'
+]
