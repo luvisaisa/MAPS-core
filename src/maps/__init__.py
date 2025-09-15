6e88ed730b414dfa2763032eb2acb0189cc3d18c
luckyisaisa <isa.lucia.sch@gmail.com>
2025-09-15 10:00:00 -0400

    feat: update package exports with new functions

--- src/maps/__init__.py
+++ src/maps/__init__.py
@@ -3,20 +3,31 @@
 Core functionality for parsing medical imaging XML annotation data.
 """
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 from .parser import (
     parse_radiology_sample,
     parse_multiple,
     export_excel,
     detect_parse_case,
-    get_expected_attributes_for_case
+    get_expected_attributes_for_case,
+    extract_roi_data,
+    extract_characteristics,
+    extract_reading_sessions,
+    get_parse_statistics
 )
+
+from .structure_detector import analyze_xml_structure
 
 __all__ = [
     'parse_radiology_sample',
     'parse_multiple',
     'export_excel',
     'detect_parse_case',
-    'get_expected_attributes_for_case'
+    'get_expected_attributes_for_case',
+    'extract_roi_data',
+    'extract_characteristics',
+    'extract_reading_sessions',
+    'get_parse_statistics',
+    'analyze_xml_structure'
 ]
